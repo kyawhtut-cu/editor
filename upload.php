4069f5d1f0f3fec8b9cd85539074cb53bd260fa6
Kyaw Htut <developer.kyawhtut@gmail.com>
2018-08-21 18:02:35 +0630

    Delete upload.php

--- upload.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$my_file = 'http://localhost/project/ace-editor/index.html';
-	$handle = fopen($my_file, "w") or die("Unable to open file!");
-	fwrite($handle, "Hello");
-	fclose($handle);
-?>
